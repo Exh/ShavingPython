ea8727593c568dfefd4e4220588a04e45cdbd993
e.tarasenko <exh@xakep.ru>
2017-04-25 17:40:24 +0300

    add test_UserSpendMoneyReturn1USD_When_SubscribingConsistOf_ShaveAsProduct_OnceAMonthAsAhippingInterval_StartDate14Jan2017_Today14Jan2017 add gitignore, remove .idea

--- project/service-tests.py
+++ project/service-tests.py
@@ -1,8 +1,9 @@
 import unittest
 
-class BlackJackTests(unittest.TestCase):
-	def test_ci_check(self):
-		self.assertEqual(True, True)
+class ShavingAccessoriesTest(unittest.TestCase):
+    def test_UserSpendMoneyReturn1USD_When_SubscribingConsistOf_ShaveAsProduct_OnceAMonthAsAhippingInterval_StartDate14Jan2017_Today14Jan2017(self):
+
+        self.assertEqual(user.spendMoney, 1)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
