afe6b171a984e1add75c7e644560170e138e502c
e.tarasenko <exh@xakep.ru>
2017-04-25 23:18:22 +0300

    ADD test_UserSpendMoneyReturn108USD_When_SubscribingConsistOf_ShaveAndGelAsProduct_OnceAMonthAsShippingInterval_StartDate14Jan2017_Today13Jan2018

--- project/service-tests.py
+++ project/service-tests.py
@@ -8,7 +8,7 @@ from datetime import date
 
 
 class ShavingAccessoriesTest(unittest.TestCase):
-    def test_UserSpendMoneyReturn1USD_When_SubscribingConsistOf_ShaveAsProduct_OnceAMonthAsAhippingInterval_StartDate14Jan2017_Today14Jan2017(self):
+    def test_UserSpendMoneyReturn1USD_When_SubscribingConsistOf_ShaveAsProduct_OnceAMonthAsShippingInterval_StartDate14Jan2017_Today14Jan2017(self):
         product = Product("Shave", 1)
         interval = OnceAMonth(14)
         user = User()
@@ -18,7 +18,7 @@ class ShavingAccessoriesTest(unittest.TestCase):
 
         self.assertEqual(user.spendCash, 1)
 
-    def test_UserSpendMoneyReturn9USD_When_SubscribingConsistOf_ShaveAndGelAsProduct_OnceAMonthAsAhippingInterval_StartDate14Jan2017_Today14Jan2017(self):
+    def test_UserSpendMoneyReturn9USD_When_SubscribingConsistOf_ShaveAndGelAsProduct_OnceAMonthAsShippingInterval_StartDate14Jan2017_Today14Jan2017(self):
         product = Product("Shave+Gel", 9)
         interval = OnceAMonth(14)
         user = User()
@@ -28,7 +28,7 @@ class ShavingAccessoriesTest(unittest.TestCase):
 
         self.assertEqual(user.spendCash, 9)
 
-    def test_UserSpendMoneyReturn2USD_When_SubscribingConsistOf_ShaveAsProduct_OnceAMonthAsAhippingInterval_StartDate14Jan2017_Today15Feb2017(self):
+    def test_UserSpendMoneyReturn2USD_When_SubscribingConsistOf_ShaveAsProduct_OnceAMonthAsShippingInterval_StartDate14Jan2017_Today15Feb2017(self):
         product = Product("Shave", 1)
         interval = OnceAMonth(14)
         user = User()
@@ -38,7 +38,7 @@ class ShavingAccessoriesTest(unittest.TestCase):
 
         self.assertEqual(user.spendCash, 2)
 
-    def test_UserSpendMoneyReturn12USD_When_SubscribingConsistOf_ShaveAsProduct_OnceAMonthAsAhippingInterval_StartDate14Jan2017_Today13Jan2018(self):
+    def test_UserSpendMoneyReturn12USD_When_SubscribingConsistOf_ShaveAsProduct_OnceAMonthAsShippingInterval_StartDate14Jan2017_Today13Jan2018(self):
         product = Product("Shave", 1)
         interval = OnceAMonth(14)
         user = User()
@@ -47,6 +47,16 @@ class ShavingAccessoriesTest(unittest.TestCase):
         subscribing.calculatePaymentTo(date(2018, 1, 13))
 
         self.assertEqual(user.spendCash, 12)
+
+    def test_UserSpendMoneyReturn108USD_When_SubscribingConsistOf_ShaveAndGelAsProduct_OnceAMonthAsShippingInterval_StartDate14Jan2017_Today13Jan2018(self):
+        product = Product("Shave+Gel", 9)
+        interval = OnceAMonth(14)
+        user = User()
+        subscribing = Subscribing(user, product, interval, date(2017, 1, 14))
+
+        subscribing.calculatePaymentTo(date(2018, 1, 13))
+
+        self.assertEqual(user.spendCash, 108)
 
 if __name__ == '__main__':
     unittest.main()
