d79a95fb8dbb2d24b0c986e370d6597545ab19d2
e.tarasenko <exh@xakep.ru>
2017-04-25 19:06:32 +0300

    add test_UserSpendMoneyReturn9USD_When_SubscribingConsistOf_ShaveAndGelAsProduct_OnceAMonthAsAhippingInterval_StartDate14Jan2017_Today14Jan2017

--- project/service-tests.py
+++ project/service-tests.py
@@ -18,5 +18,16 @@ class ShavingAccessoriesTest(unittest.TestCase):
 
         self.assertEqual(user.spendCash, 1)
 
+    def test_UserSpendMoneyReturn9USD_When_SubscribingConsistOf_ShaveAndGelAsProduct_OnceAMonthAsAhippingInterval_StartDate14Jan2017_Today14Jan2017(self):
+        product = Product("Shave+Gel", 9)
+        interval = OnceAMonth(14)
+        user = User()
+        subscribing = Subscribing(user, product, interval, date(2017, 1, 14))
+
+        subscribing.calculatePayment()
+
+        self.assertEqual(user.spendCash, 9)
+
+
 if __name__ == '__main__':
     unittest.main()
